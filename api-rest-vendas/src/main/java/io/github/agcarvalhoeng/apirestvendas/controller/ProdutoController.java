8396558c8393c71be0768deb52b61efa7bef0f8d
Adriano <agcarvalho@ifto.edu.br>
2023-11-13 20:10:24 -0300

    Segunda versão

--- api-rest-vendas/src/main/java/io/github/agcarvalhoeng/apirestvendas/controller/ProdutoController.java
+++ api-rest-vendas/src/main/java/io/github/agcarvalhoeng/apirestvendas/controller/ProdutoController.java
@@ -1,14 +1,25 @@
 package io.github.agcarvalhoeng.apirestvendas.controller;
 
+import io.github.agcarvalhoeng.apirestvendas.model.Produto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Coc = Convention over Configuration (convenção
+ * no lugar de configuração)
+ *
+ */
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
-    @GetMapping("/ola")
-    public String ola(){
-        return "Olá";
+    /**
+     * Métodos do controller são chamados de endpoints (endereço
+     * final - completo).
+     * @return
+     */
+    @GetMapping()
+    public Produto findById(){
+        return new Produto(0L, "TV", "TV LCD", 1000, 100);
     }
 }
---
+++ api-rest-vendas/src/main/java/io/github/agcarvalhoeng/apirestvendas/model/Produto.java
@@ -0,0 +1,19 @@
+package io.github.agcarvalhoeng.apirestvendas.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**As anotações abaixo são para criar getters e setters, construtores com
+*todos os parâmetros e sem aprâmetro (este último por conta da JPA).
+ **/
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class Produto {
+    //Declarando as variáveis a srem utilizadas.
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private double preco;
+    private int estoque;
+}
