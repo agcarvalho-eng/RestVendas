2a966b20f1735c922f3344263e0c9c755ce5913d
Adriano <agcarvalho@ifto.edu.br>
2023-11-13 20:10:24 -0300

    Segunda versão com comentários.

--- api-rest-vendas/src/main/java/io/github/agcarvalhoeng/apirestvendas/controller/ProdutoController.java
+++ api-rest-vendas/src/main/java/io/github/agcarvalhoeng/apirestvendas/controller/ProdutoController.java
@@ -1,14 +1,41 @@
 package io.github.agcarvalhoeng.apirestvendas.controller;
 
+import io.github.agcarvalhoeng.apirestvendas.model.Produto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Coc = Convention over Configuration (convenção
+ * no lugar de configuração)
+ *
+ */
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
-    @GetMapping("/ola")
-    public String ola(){
-        return "Olá";
+    /**
+     * Métodos do controller são chamados de endpoints (endereço
+     * final - completo).
+     * @return
+     */
+
+    /**Método para buscar o Id do produto.
+     * No GetMapping tem-se o endereço (URL) e entre chaves o parâmetro que irá receber.
+     * @param id
+     * @return
+     */
+    @GetMapping("/findById/{id}")
+    public Produto findById(Long id){
+        return new Produto(id, "TV","123", "TV LCD", 1000, 100);
+    }
+
+    /**Método para buscar o EAN (código de barras) do produto.
+     * No GetMapping tem-se o endereço (URL) e entre chaves o parâmetro que irá receber.
+     * @param ean
+     * @return
+     */
+    @GetMapping("/findByEAN/{ean}")
+    public Produto findByEAN(String ean){
+        return new Produto(0L, "TV","123", "TV LCD", 1000, 100);
     }
 }
---
+++ api-rest-vendas/src/main/java/io/github/agcarvalhoeng/apirestvendas/model/Produto.java
@@ -0,0 +1,21 @@
+package io.github.agcarvalhoeng.apirestvendas.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**As anotações abaixo são para criar getters e setters, construtores com
+*todos os parâmetros e sem aprâmetro (este último por conta da JPA).
+ **/
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class Produto {
+    //Declarando as variáveis a srem utilizadas.
+    private Long id;
+    private String titulo;
+    //Criando uma variável para buscar código de barras.
+    private String ean;
+    private String descricao;
+    private double preco;
+    private int estoque;
+}
