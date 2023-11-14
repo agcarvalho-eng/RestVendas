1f188094a6a7287014c0ce0d58f14f034b2543ed
Adriano <agcarvalho@ifto.edu.br>
2023-11-13 22:01:00 -0300

    Acrescentado os insert, delete, get, update e outros.

--- api-rest-vendas/src/main/java/io/github/agcarvalhoeng/apirestvendas/controller/ProdutoController.java
+++ api-rest-vendas/src/main/java/io/github/agcarvalhoeng/apirestvendas/controller/ProdutoController.java
@@ -1,10 +1,14 @@
 package io.github.agcarvalhoeng.apirestvendas.controller;
 
 import io.github.agcarvalhoeng.apirestvendas.model.Produto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.agcarvalhoeng.apirestvendas.repository.ProdutoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 /**
  * Coc = Convention over Configuration (convenção
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/produto")
+@AllArgsConstructor //Cria o construtor automaticamente (para por exemplo o repository).
 public class ProdutoController {
     /**
      * Métodos do controller são chamados de endpoints (endereço
@@ -24,14 +29,42 @@ public class ProdutoController {
 
     /**Método para buscar o Id do produto.
      * No GetMapping tem-se o endereço (URL) e entre chaves o parâmetro que irá receber.
+     * @para id
+     * @return
+     */
+    private final ProdutoRepository repository;
+    @PutMapping("/{id}")
+    public void update(@PathVariable long id, @RequestBody Produto produto){
+        repository.save(produto);
+    }
+    @PostMapping
+    public long insert(@RequestBody Produto produto){
+        return repository.save(produto).getId();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id){
+        repository.deleteById(id);
+    }
+     @GetMapping
+    // @PathVariable = vamos utilizar Path Param.
+    public List<Produto> findAll(){
+        return repository.findAll();
+                /**List.of(new Produto(1L, "TV","123", "TV LCD", 1000, 100,"Palmas"),
+                new Produto(2L, "TV", "123", "TV LCD", 1000, 100, "Palmas"));**/
+    }
+
+    /**Método para buscar o Id do produto.
+     * No GetMapping tem-se o endereço (URL) e entre chaves o parâmetro que irá receber.
      * @param id
      * @return
      */
-
     @GetMapping("/{id}")
     // @PathVariable = vamos utilizar Path Param.
-    public Produto findById(@PathVariable Long id){
-        return new Produto(id, "TV","123", "TV LCD", 1000, 100,"Palmas");
+    public Produto findById(@PathVariable final Long id){
+        return repository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND));//Já fazendo o tratamento do erro.
+                /**new Produto(id, "TV","123", "TV LCD", 1000, 100,"Palmas");**/
     }
 
     /**Método para buscar o EAN (código de barras) do produto.
@@ -42,6 +75,9 @@ public class ProdutoController {
 
     @GetMapping("/{loja}/{ean}")
     public Produto findByEAN(@PathVariable String loja, @PathVariable String ean){
-        return new Produto(0L, "TV", ean, "TV LCD", 1000, 100, loja);
+        return repository.findByEanAndLoja(ean, loja).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND));//Já fazendo o tratamento do erro.
+                /**new Produto(0L, "TV", ean, "TV LCD", 1000, 100, loja);**/
     }
+
 }
---
+++ api-rest-vendas/src/main/java/io/github/agcarvalhoeng/apirestvendas/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package io.github.agcarvalhoeng.apirestvendas.repository;
+
+import io.github.agcarvalhoeng.apirestvendas.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    Optional<Produto> findByEanAndLoja(String ean, String loja);
+}
