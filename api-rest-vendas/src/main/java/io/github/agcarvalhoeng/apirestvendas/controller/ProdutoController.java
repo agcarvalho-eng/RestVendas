25b5742d9c7b6b959bb7f339d289162279d5d891
Adriano <agcarvalho@ifto.edu.br>
2023-11-13 22:01:00 -0300

    Acrescentado as anotações e configurações.

--- api-rest-vendas/src/main/java/io/github/agcarvalhoeng/apirestvendas/controller/ProdutoController.java
+++ api-rest-vendas/src/main/java/io/github/agcarvalhoeng/apirestvendas/controller/ProdutoController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * Coc = Convention over Configuration (convenção
  * no lugar de configuração)
@@ -24,10 +26,22 @@ public class ProdutoController {
 
     /**Método para buscar o Id do produto.
      * No GetMapping tem-se o endereço (URL) e entre chaves o parâmetro que irá receber.
-     * @param id
+     * @para id
      * @return
      */
 
+    @GetMapping()
+    // @PathVariable = vamos utilizar Path Param.
+    public List<Produto> findAll(){
+        return List.of(new Produto(1L, "TV","123", "TV LCD", 1000, 100,"Palmas"),
+                new Produto(2L, "TV", "123", "TV LCD", 1000, 100, "Palmas"));
+    }
+
+    /**Método para buscar o Id do produto.
+     * No GetMapping tem-se o endereço (URL) e entre chaves o parâmetro que irá receber.
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
     // @PathVariable = vamos utilizar Path Param.
     public Produto findById(@PathVariable Long id){
@@ -44,4 +58,5 @@ public class ProdutoController {
     public Produto findByEAN(@PathVariable String loja, @PathVariable String ean){
         return new Produto(0L, "TV", ean, "TV LCD", 1000, 100, loja);
     }
+
 }
--- api-rest-vendas/src/main/java/io/github/agcarvalhoeng/apirestvendas/model/Produto.java
+++ api-rest-vendas/src/main/java/io/github/agcarvalhoeng/apirestvendas/model/Produto.java
@@ -1,5 +1,12 @@
 package io.github.agcarvalhoeng.apirestvendas.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,13 +16,27 @@ import lombok.Setter;
 *todos os parâmetros e sem aprâmetro (este último por conta da JPA).
  **/
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
+//Transformando a classe em uma entidade (vai ser uma tabela no banco de dados.
+@Entity
 public class Produto {
+    /**
+     * @Id = informando que o "id" será nossa chave primária no banco de dados.
+     * @Generated = vai gerar automaticamente o id do tipo padrão do banco de dados (Identity).
+     * @notNull = não pode ser nulo.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
     //Declarando as variáveis a srem utilizadas.
     private Long id;
+    @NotBlank
+    //@NotBlank = os campos não podem ser branco ou nulo.
     private String titulo;
     //Criando uma variável para buscar código de barras.
     private String ean;
     private String descricao;
+    @Min(1)
+    //@Min = os campos terão valor mínimo 1.
     private double preco;
     private int estoque;
     private String loja;
